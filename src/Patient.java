d2006c12dd4b92b7f8c0b3e2228115a88f88b39d
Tayou <tayou@gmx.net>
2020-04-03 10:45:12 +0200

    added patient

--- src/Patient.java
+++ src/Patient.java
@@ -1,2 +1,15 @@
 public class Patient {
+    int id;
+    int age;
+    String name;
+
+    public Patient(String name, int id, int age) {
+        this.id = id;
+        this.age = age;
+        this.name = name;
+    }
+
+    public void read() {}
+
+    public void display() {}
 }
