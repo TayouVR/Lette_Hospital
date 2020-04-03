10337f2b720a85fdcc73e14657dbbf7e3eba6513
Tayou <tayou@gmx.net>
2020-04-03 10:41:27 +0200

    added station

--- src/Station.java
+++ src/Station.java
@@ -1,2 +1,46 @@
+import java.util.ArrayList;
+
 public class Station {
+    private String name;
+    private ArrayList<Patient> patients;
+    private int anzPatienten;
+
+    public Station(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addPatient(Patient patient) {
+        patients.add(patient);
+        anzPatienten++;
+    }
+
+    public boolean searchPatient(String name) {
+        for (Patient s: patients) {
+            if (s.getName().equals(name)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean deletePatient(String name) {
+        for (Patient s: patients) {
+            if (s.getName().equals(name)) {
+                patients.remove(s);
+                anzPatienten--;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void displayAll() {
+        for (Patient s: patients) {
+            System.out.println(s);
+        }
+    }
 }
